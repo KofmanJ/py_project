a9f16d2191b5a9608ef88953e0ba1a39da7da228
Julia_Kofman <juliakofmana@mail.ru>
2023-09-17 10:33:16 +0300

    add file

---
+++ funcs.py
@@ -0,0 +1,9 @@
+def summ_numbers(num_1, num_2):
+    return num_1 + num_2
+
+# print(summ_numbers(1, 2))
+
+def diff_numbers(num_1, num_2):
+    return num_1 - num_2
+
+# print(diff_numbers(1, 2))
---
+++ main.py
@@ -0,0 +1,4 @@
+from funcs import summ_numbers, diff_numbers
+
+print(summ_numbers(1, 2))
+print(diff_numbers(1, 2))
